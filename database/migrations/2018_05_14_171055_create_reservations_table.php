390be69825fa803c7e844e94e16c5ad47267bd9b
manjurulhoque <rumimanzurulhoque@gmail.com>
2018-05-14 23:15:41 +0600

    reservation model, controller and migration

---
+++ database/migrations/2018_05_14_171055_create_reservations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unisigned();
+            $table->integer('room_id')->unisigned();
+            $table->dateTime('start_date');
+            $table->dateTime('end_date');
+            $table->integer('price');
+            $table->integer('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
